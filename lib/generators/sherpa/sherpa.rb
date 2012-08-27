24f3257004d97e6a7a071c0521ce064d8d77f296
Matthew Kitt <mk@mkitt.net>
2012-08-27 01:00:38 -0600

    Move the sherpa stuff over to generators for now

--- lib/generators/sherpa/sherpa.rb
+++ lib/generators/sherpa/sherpa.rb
@@ -22,7 +22,7 @@ module Sherpa
                                           :superscript => true,
                                           :space_after_headers => true
                                          )
-      @stache_layout = File.read(File.join("./bin", "layout.mustache"))
+      @stache_layout = File.read(File.join("./lib/generators/sherpa/", "layout.mustache"))
 
       manifest = Dir["./app/assets/**/*.md"]
       lines = cat_manifest(manifest)
