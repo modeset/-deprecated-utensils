59e116f05b6a17b044ec3c03b7c0541d52a4411b
Matthew Kitt <mk@mkitt.net>
2012-08-31 13:36:37 -0600

    Quiet log errors in the cli, tabularize that shit

--- lib/roos/application.rb
+++ lib/roos/application.rb
@@ -14,19 +14,19 @@ module Roos
     config.secret_token = '5df96576e3993124e11fa574f45b0704401b333941e356a414eff39cade37040e81988de770e8e580a63903f3a61c96c019c9ca0283a4c19d49e0967ec9ce6cc'
 
     config.consider_all_requests_local       = true
-
-    config.encoding         = 'utf-8'
-    config.assets.enabled   = true
-    config.assets.version   = '1.0'
-    config.assets.paths    << 'app/assets'
-    config.assets.paths    << 'app/assets/components'
-    config.assets.paths    << 'app/assets/application/javascripts'
-    config.assets.paths    << 'spec/javascripts'
-    config.assets.compile       = true
-    config.serve_static_assets  = true
-    config.assets.prefix        = 'assets'
-    config.assets.digest        = false
-    config.assets.compress      = false
+    config.encoding                          = 'utf-8'
+    config.assets.enabled                    = true
+    config.assets.version                    = '1.0'
+    config.assets.paths                      << 'app/assets'
+    config.assets.paths                      << 'app/assets/components'
+    config.assets.paths                      << 'app/assets/application/javascripts'
+    config.assets.paths                      << 'spec/javascripts'
+    config.assets.compile                    = true
+    config.serve_static_assets               = true
+    config.assets.prefix                     = 'assets'
+    config.assets.digest                     = false
+    config.assets.compress                   = false
+    config.active_support.deprecation        = :log
 
     # Configure assets for production
     unless Rails.env.production?
