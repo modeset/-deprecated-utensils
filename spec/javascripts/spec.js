2bd564576b6a6939de50704f8977eb9631a7d981
Matthew Kitt <mk@mkitt.net>
2012-09-18 16:01:28 -0600

    Move spec.js.coffee to spec.js
    This keeps it in the same form as application.js and easier copying
    over when creating new components

---
+++ spec/javascripts/spec.js
@@ -0,0 +1,17 @@
+//= require jquery
+//= require underscore
+
+//= require utensil/spec
+//= require bindable/spec
+//= require detect/spec
+//= require togglable/spec
+//= require togglable_group/spec
+//= require directional/spec
+//= require timeslot/spec
+
+//= require tip/spec
+//= require pop/spec
+//= require dimensionizer/spec
+
+//= require_tree ./
+
