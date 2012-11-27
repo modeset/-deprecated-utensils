08b286748602928ad269a9f786320f6cbe3e9f5a
Matthew Kitt <mk@mkitt.net>
2012-11-26 22:49:33 -0700

    Let docomo.js/sass just source application.js/sass

--- spec/dummy/app/assets/javascripts/docomo.js
+++ spec/dummy/app/assets/javascripts/docomo.js
@@ -1,24 +1,3 @@
 //= require jquery.min
-//= require utensils/utensils
-//= require utensils/bindable
-//= require utensils/triggerable
-//= require utensils/toggle
-//= require utensils/toggle_group
-//= require utensils/dismiss
-//= require utensils/collapse
+//= require application
 
-//= require utensils/carousel
-//= require utensils/carousel_touch
-//= require utensils/carousel_touch_infinite
-//= require utensils/modal
-//= require utensils/drop
-//= require utensils/tab
-//= require utensils/tip
-//= require utensils/pop
-//= require utensils/progress
-//= require utensils/slider
-
-// Lets get this party started..
-$(function() {
-  utensils._binder = new utensils.Bindable().bindAll();
-});
