9f09fe7aad1fe3e8c16125e72a0dec60239dedd6
Matthew Kitt <mk@mkitt.net>
2013-02-19 15:15:38 -0700

    Have the install generator pull from the dummy app
    Fixes #5

--- lib/generators/utensils/install/install_generator.rb
+++ lib/generators/utensils/install/install_generator.rb
@@ -2,39 +2,46 @@ module Utensils
   module Generators
     class InstallGenerator < Rails::Generators::Base
       desc 'Install the base assets for setting up a new project using utensils.'
-      source_root File.expand_path('../templates', __FILE__)
+      source_root File.expand_path('../../../../../spec/dummy', __FILE__)
+
+      def cleanup_assets
+        remove_file "app/assets/stylesheets/application.css"
+        remove_file "app/assets/javascripts/application.js"
+        remove_dir "public"
+      end
 
       def create_sass_files
-        copy_file 'application.sass', 'app/assets/stylesheets/application.sass'
-        copy_file 'spec/dummy/app/assets/stylesheets/_config.sass', 'app/assets/stylesheets/_config.sass'
-        copy_file 'spec/dummy/app/assets/stylesheets/dinosaur.sass', 'app/assets/stylesheets/dinosaur.sass'
+        copy_file 'app/assets/stylesheets/_config.sass'
+        copy_file 'app/assets/stylesheets/application.sass'
+        copy_file 'app/assets/stylesheets/dinosaur.sass'
       end
 
       def create_coffee_files
-        copy_file 'application.coffee', 'app/assets/javascripts/application.coffee'
-        copy_file 'spec/dummy/app/assets/javascripts/dinosaur.coffee', 'app/assets/javascripts/dinosaur.coffee'
-        copy_file 'spec/dummy/app/assets/javascripts/polyfill.coffee', 'app/assets/javascripts/polyfill.coffee'
+        copy_file 'app/assets/javascripts/application.coffee'
+        copy_file 'app/assets/javascripts/dinosaur.coffee'
+        copy_file 'app/assets/javascripts/polyfill.coffee'
       end
 
       def create_haml_files
-        copy_file 'spec/dummy/app/views/layouts/pages.html.haml', 'app/views/layouts/application.html.haml'
+        copy_file 'app/views/layouts/pages.html.haml', 'app/views/layouts/application.html.haml'
         empty_directory 'app/views/layouts/shared'
-        copy_file 'spec/dummy/app/views/layouts/shared/_flash_message.html.haml', 'app/views/layouts/shared/_flash_message.html.haml'
-        copy_file 'spec/dummy/app/views/layouts/shared/_upgrade.html.haml', 'app/views/layouts/shared/_upgrade.html.haml'
+        copy_file 'app/views/layouts/shared/_flash_message.html.haml'
+        copy_file 'app/views/layouts/shared/_upgrade.html.haml'
       end
 
       def create_public_assets
-        copy_file 'spec/dummy/public/404.html', 'public/404.html'
-        copy_file 'spec/dummy/public/422.html', 'public/422.html'
-        copy_file 'spec/dummy/public/500.html', 'public/500.html'
-        copy_file 'spec/dummy/public/apple-touch-icon-114x114-precomposed.png', 'public/apple-touch-icon-114x114-precomposed.png'
-        copy_file 'spec/dummy/public/apple-touch-icon-57x57-precomposed.png', 'public/apple-touch-icon-57x57-precomposed.png'
-        copy_file 'spec/dummy/public/apple-touch-icon-72x72-precomposed.png', 'public/apple-touch-icon-72x72-precomposed.png'
-        copy_file 'spec/dummy/public/apple-touch-icon-precomposed.png', 'public/apple-touch-icon-precomposed.png'
-        copy_file 'spec/dummy/public/apple-touch-icon.png', 'public/apple-touch.png'
-        copy_file 'spec/dummy/public/error.html', 'public/error.html'
-        copy_file 'spec/dummy/public/favicon.ico', 'public/favicon.ico'
-        copy_file 'spec/dummy/public/maintenance.html', 'public/maintenance.html'
+        empty_directory 'public'
+        copy_file 'public/404.html'
+        copy_file 'public/422.html'
+        copy_file 'public/500.html'
+        copy_file 'public/apple-touch-icon-114x114-precomposed.png'
+        copy_file 'public/apple-touch-icon-57x57-precomposed.png'
+        copy_file 'public/apple-touch-icon-72x72-precomposed.png'
+        copy_file 'public/apple-touch-icon-precomposed.png'
+        copy_file 'public/apple-touch-icon.png'
+        copy_file 'public/error.html'
+        copy_file 'public/favicon.ico'
+        copy_file 'public/maintenance.html'
       end
 
     end
--- lib/generators/utensils/install/templates/application.coffee
+++
@@ -1,7 +0,0 @@
-#= require utensils/utensils
-#= require utensils/bindable
-
-# Lets get this party started..
-$ ->
-  utensils._binder = new utensils.Bindable().bindAll()
-
